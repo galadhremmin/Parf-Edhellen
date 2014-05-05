36d543f1ad3857f9ada06a17f20b04ca9741bab3
galadhremmin <zanathel@gmail.com>
2014-05-05 09:27:54 -0700

    - Fixed an error which caused the javascript consolidation feature to malfunction on iOS - Minifies javascripts

--- js/js.php
+++ js/js.php
@@ -1,33 +1,38 @@
 <?php
-  $filter = null;
-  if (isset($_GET['filter']) && preg_match('/^[a-z0-9\\-]+$/i', $_GET['filter'])) {
-    $filter = str_replace('-', '\\-', $_GET['filter']);
-  }
-  
-  $files = scandir('.');
-  sort($files);
-  
+  define('CONSOLIDATED_FILE_NAME', '../cache/_consolidate.js');
+  define('CONSOLIDATION_LIFETIME', 60*60*24*7*30);
+
+  include_once '../lib/system.php';
+
   $js = '';
-  foreach ($files as $file) {
-    if ($file == '.' || $file == '..')
-      continue;
-    
-    $info = pathinfo($file);
-    
-    if ($info['extension'] == 'js') {
-      if ($filter == null || ($filter != null && preg_match('/^[0-9]+\\.'.$filter.'$/', $info['filename']))) {
-        $js .= file_get_contents($file)."\n\n";
+  
+  if (!file_exists(CONSOLIDATED_FILE_NAME) || time() - filemtime(CONSOLIDATED_FILE_NAME) > CONSOLIDATION_LIFETIME) {
+    $files = array();
+    $fileIt = new DirectoryIterator('.');
+    foreach ($fileIt as $it) {
+      if ($fileIt->getExtension() === 'js') {
+        $files[] = $fileIt->getFilename();
       }
     }
+    
+    sort($files);
+    
+    foreach ($files as $file) {
+      $js .= file_get_contents($file)."\n";
+    }
+    
+    $js = JSMin::minify($js);
+    file_put_contents(CONSOLIDATED_FILE_NAME, $js);
+    
+  } else {
+    $js = file_get_contents(CONSOLIDATED_FILE_NAME);
   }
   
   $expires = 60*60*24*14;
   
   header("Content-Type: text/javascript; charset=utf-8");
-  header("Content-Length: ".strlen($js));
   header("Pragma: public");
   header("Cache-Control: maxage=".$expires);
   header('Expires: ' . gmdate('D, d M Y H:i:s', time() + $expires) . ' GMT');
 
   echo $js;
-?>
