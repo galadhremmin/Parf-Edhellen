495324ada89decda038e2929b8aa224217ab01ab
galadhremmin <zanathel@gmail.com>
2017-03-17 18:16:09 +0100

    - Removed deprecated code

--- parf-edhellen/app/Models/Sense.php
+++ parf-edhellen/app/Models/Sense.php
@@ -13,29 +13,4 @@ class Sense extends Model
     public function word() {
         return $this->hasOne(Word::class, 'KeyID', 'IdentifierID');
     }
-
-    public function scopeFindForWord($query, $normalizedWord) {
-        if (strpos($normalizedWord, '*') === false) {
-            $normalizedWord .= '%';
-        } else {
-            $normalizedWord = str_replace('*', '%', $normalizedWord);
-        }
-
-        $peripheralNamespaces = DB::table('keywords as k')
-            ->distinct()
-            ->join('translation as t', 'k.TranslationID', '=', 't.TranslationID')
-            ->where([
-                ['k.NormalizedKeyword', 'like', $normalizedWord],
-                ['t.Deleted', '=', 0]
-            ])
-            ->select('k.NamespaceID');
-
-
-        return $query
-            ->join('keywords', 'namespace.NamespaceID', 'keywords.NamespaceID')
-            ->where('NormalizedKeyword', 'like', $normalizedWord)
-            ->select('namespace.NamespaceID')
-            ->union($peripheralNamespaces);
-
-    }
 }
