5b09841bf1bfaee07fda8eb993fabac19693f7fa
galadhremmin <zanathel@gmail.com>
2017-04-27 18:24:55 +0200

    Bug fix
    - Fixed a typo error

--- src/resources/assets/js/_shared/form.js
+++ src/resources/assets/js/_shared/form.js
@@ -56,15 +56,15 @@ export class EDStatefulFormComponent extends React.Component {
             const parts = name.split('.');
             name = parts[0];
             
-            let affectedObject = this.state[name] || {};
+            let ptr = this.state[name] || {};
             for (let i = 1; i < parts.length - 1; i += 1) {
-                letffectedObject[parts[i]] = affectedObject[parts[i]] || {};
+                ptr[parts[i]] = ptr[parts[i]] || {};
 
-                affectedObject = affectedObject[parts[i]];
+                ptr = ptr[parts[i]];
             }
 
-            affectedObject[parts[parts.length - 1]] = value;
-            value = affectedObject;
+            ptr[parts[parts.length - 1]] = value;
+            value = ptr;
         } 
 
         this.setState({
