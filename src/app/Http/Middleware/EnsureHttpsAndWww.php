8e835916997bf95707c13e8294f2d72d775e677d
galadhremmin <zanathel@gmail.com>
2017-05-05 02:24:10 +0200

    Ensure HTTPS and proper prefix (www or not?)

---
+++ src/app/Http/Middleware/EnsureHttpsAndWww.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use \Illuminate\Auth\AuthenticationException;
+use \Illuminate\Contracts\Foundation\Application;
+use Closure;
+
+class EnsureHttpsAndWww
+{
+    protected $_appUrl;
+    protected $_expects;
+    protected $_expectsLength;
+    protected $_isSecure;
+
+    public function __construct(Application $app) {
+        $appUrl = config('app.url');
+        $parts = parse_url($appUrl);
+
+        $this->_appUrl        = $appUrl;
+        $this->_expects       = $parts['scheme'] . '://' . $parts['host'];
+        $this->_expectsLength = strlen($this->_expects);
+        $this->_isSecure      = $parts['scheme'] === 'https';
+    } 
+
+    /**
+     * Ensures that the request is handled over HTTPS.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  $role
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // retrieve the first part of the URL, which should match the expected prefix
+        $urlPart = substr($request->fullUrl(), 0, $this->_expectsLength);
+
+        if ($urlPart !== $this->_expects) {
+            $correctUrl = $this->_appUrl.'/'.$request->path();
+
+            if (!$this->_isSecure) {
+                return redirect()->to($correctUrl);
+            }
+
+            // for Proxies
+            Request::setTrustedProxies([$request->getClientIp()]);
+            return redirect()->secure($correctUrl);
+        }
+
+        return $next($request);
+    }
+
+}
--- src/config/ed.php
+++ src/config/ed.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
+    // optional footer
     'footer_view' => env('ED_FOOTER_INCLUDE', ''),
 ];
