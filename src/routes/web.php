06f1d5744fa44f95e68d609f7e280c00214e4483
Aldaleon <zanathel@gmail.com>
2017-07-17 10:24:50 +0200

    Throttling error management and markdown parser

--- src/routes/web.php
+++ src/routes/web.php
@@ -94,12 +94,19 @@ Route::group([
 
     Route::get('inflection/{id?}',               [ 'uses' => 'InflectionApiController@index' ]);
 
-    Route::post('utility/markdown',              [ 'uses' => 'UtilityApiController@parseMarkdown' ]);
-    Route::post('utility/error',                 [ 'uses' => 'UtilityApiController@logError' ]);
-
     Route::resource('forum', 'ForumApiController', ['only' => [
         'index', 'show'
     ]]);
+});
+
+Route::group([ 
+        'namespace'  => 'Api\v1', 
+        'prefix'     => 'api/v1',
+        'middleware' => 'throttle'
+    ], function () {
+
+    Route::post('utility/markdown',              [ 'uses' => 'UtilityApiController@parseMarkdown' ]);
+    Route::post('utility/error',                 [ 'uses' => 'UtilityApiController@logError' ]);
 });
 
 // API
