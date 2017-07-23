86f41c77afe67e72f549689e7c3c6adbcdf5befa
Aldaleon <zanathel@gmail.com>
2017-07-23 13:52:44 +0200

    Added type column for sentence fragments

--- model/database-1.9.93.sql
+++ model/database-1.9.93.sql
@@ -1,3 +1,10 @@
-ALTER TABLE `sentence_fragments` ADD `is_excluded` int(1) NOT NULL DEFAULT 0;
+ALTER TABLE `sentence_fragments` ADD `type` int(3) NOT NULL DEFAULT 0;
+
+UPDATE `sentence_fragments` SET `type` = 10 -- linebreak
+  WHERE `is_linebreak` = 1;
+UPDATE `sentence_fragments` SET `type` = 31 -- unit separator = interpunctuation
+  WHERE `is_linebreak` = 0 AND `fragment` in( '!', '.', ',', '?' );
+UPDATE `sentence_fragments` SET `type` = 45 -- hyphen = word connection
+  WHERE `is_linebreak` = 0 AND `fragment` in( '-', '·' );
 
 INSERT INTO `version` (`number`, `date`) VALUES (1.993, NOW());
