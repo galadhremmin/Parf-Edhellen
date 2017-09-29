e36ecdae2b5a373cbc3e025a7d0c95234aaf5d69
galadhremmin <zanathel@gmail.com>
2017-09-29 13:58:43 +0200

    Fixed redunant audit trail

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 
 use App\Models\{ Account, AuditTrail };
@@ -68,78 +69,88 @@ class AuthorController extends Controller
         
         if ($request->hasFile('avatar')) {
             $file = $request->file('avatar');
-            
-            $valid = $file->isValid();
-            $size  = false;
-            if ($valid) {
-                $size = getimagesize($file->path());
-                $valid = $size !== false && $size[0] > 0 && $size[1] > 0;
-            }
-
-            if ($valid) {
-                list($width, $height) = $size;
-
-                $factor = config('ed.avatar_size') / max($width, $height);
-
-                $newWidth  = ceil($width * $factor);
-                $newHeight = ceil($height * $factor);
-
-                try {
-                    // Read the original image into memory, and scale it to its destination size + 1.
-                    // The extra 'bleed' is used to remedy a scaling bug in PHP which results in a black
-                    // border in the lower as well as the right corner of the image.
-                    $original = imagecreatefromstring(file_get_contents($file->path()));
-                    $avatar   = imagescale($original, $newWidth + 1, $newHeight + 1, IMG_BICUBIC);
-
-                    // Having scaled the image (using the bicubic algorithm), remove the 'bleed' and 
-                    // compose the final avatar.
-                    $finalAvatar = imagecreatetruecolor($newWidth, $newHeight);
-                    imagecopyresized($finalAvatar, $avatar, 0, 0, 0, 0, $newWidth, $newHeight, $newWidth, $newWidth);
-
-                    // Turn the avatar into a string. There is no known save into memory option in GD
-                    // at the time when this was developed, thus use the output buffer to achive the
-                    // same effect.
-                    ob_start();
-                    imagepng($finalAvatar);
-                    $avatarAsString = ob_get_clean();
-
-                    Storage::disk('local')->put('public/avatars/'.$author->id.'.png', $avatarAsString);
-                    
-                    $author->has_avatar = true;
-                } catch (Exception $ex) {
-                    // Images can't be processed, so bail
-                    $author->has_avatar = false;
-                } finally {
-                    // Ensure to always free up memory.
-                    if (is_resource($original)) {
-                        imagedestroy($original);
-                    }
-                    
-                    if (is_resource($avatar)) {
-                        imagedestroy($avatar);
-                    }
-
-                    if (is_resource($finalAvatar)) {
-                        imagedestroy($finalAvatar);
-                    }
-                }
-            }
-
-            unlink($file->path());
-
-            // Register an audit trail for the changed avatar
-            $this->_auditTrail->store(AuditTrail::ACTION_PROFILE_EDIT_AVATAR, $author);
+            $this->uploadProfile($author, $file);
         }
 
         $author->nickname = $request->input('nickname');
         $author->tengwar  = $request->input('tengwar');
         $author->profile  = $request->input('profile');
-        $author->save();
 
-        // Register an audit trail for the changed profile
-        $this->_auditTrail->store(AuditTrail::ACTION_PROFILE_EDIT, $author);
+        $changed = $author->isDirty();
+        if ($changed) {
+            $author->save();
+
+            // Register an audit trail for the changed profile
+            $this->_auditTrail->store(AuditTrail::ACTION_PROFILE_EDIT, $author);
+        }
 
         return redirect()->route('author.my-profile');
+    }
+
+    private function uploadProfile(Account $author, UploadedFile $file)
+    {
+        $valid = $file->isValid();
+        $size  = false;
+        if ($valid) {
+            $size = getimagesize($file->path());
+            $valid = $size !== false && $size[0] > 0 && $size[1] > 0;
+        }
+
+        if ($valid) {
+            list($width, $height) = $size;
+
+            $factor = config('ed.avatar_size') / max($width, $height);
+
+            $newWidth  = ceil($width * $factor);
+            $newHeight = ceil($height * $factor);
+
+            try {
+                // Read the original image into memory, and scale it to its destination size + 1.
+                // The extra 'bleed' is used to remedy a scaling bug in PHP which results in a black
+                // border in the lower as well as the right corner of the image.
+                $original = imagecreatefromstring(file_get_contents($file->path()));
+                $avatar   = imagescale($original, $newWidth + 1, $newHeight + 1, IMG_BICUBIC);
+
+                // Having scaled the image (using the bicubic algorithm), remove the 'bleed' and 
+                // compose the final avatar.
+                $finalAvatar = imagecreatetruecolor($newWidth, $newHeight);
+                imagecopyresized($finalAvatar, $avatar, 0, 0, 0, 0, $newWidth, $newHeight, $newWidth, $newWidth);
+
+                // Turn the avatar into a string. There is no known save into memory option in GD
+                // at the time when this was developed, thus use the output buffer to achive the
+                // same effect.
+                ob_start();
+                imagepng($finalAvatar);
+                $avatarAsString = ob_get_clean();
+
+                Storage::disk('local')->put('public/avatars/'.$author->id.'.png', $avatarAsString);
+                
+                $author->has_avatar = true;
+            } catch (Exception $ex) {
+                // Images can't be processed, so bail
+                $author->has_avatar = false;
+            } finally {
+                // Ensure to always free up memory.
+                if (isset($original) && is_resource($original)) {
+                    imagedestroy($original);
+                }
+                
+                if (isset($avatar) && is_resource($avatar)) {
+                    imagedestroy($avatar);
+                }
+
+                if (isset($finalAvatar) && is_resource($finalAvatar)) {
+                    imagedestroy($finalAvatar);
+                }
+            }
+
+            $author->save();
+        }
+
+        unlink($file->path());
+
+        // Register an audit trail for the changed avatar
+        $this->_auditTrail->store(AuditTrail::ACTION_PROFILE_EDIT_AVATAR, $author);
     }
 
     private function getAccount(Request $request, $id)
