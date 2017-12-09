bd3209b58e1a5969a02b5d6d3e4570ad34d86bf9
galadhremmin <zanathel@gmail.com>
2017-12-09 22:55:27 +0100

    Fixed a mass assignment exception
    - Role -name- needs to be mass-assignable (fillable)

--- src/app/Models/Role.php
+++ src/app/Models/Role.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 class Role extends ModelBase
 {
+    protected $fillable = ['name'];
+
     public function scopeForAccount($query, Account $account) 
     {
         $query->join('account_role_rels as ag', 'id', '=', 'ag.role_id')
