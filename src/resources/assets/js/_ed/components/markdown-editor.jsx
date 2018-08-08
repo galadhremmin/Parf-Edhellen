368b697b4788fd232a5f22ebd33ff710dacb079f
galadhremmin <zanathel@gmail.com>
2018-08-07 20:38:30 -0700

    Added formatting help as a tab

--- src/resources/assets/js/_ed/components/markdown-editor.jsx
+++ src/resources/assets/js/_ed/components/markdown-editor.jsx
@@ -5,6 +5,7 @@ import { Parser as HtmlToReactParser } from 'html-to-react';
 
 const MDMarkdownEditTab = 0;
 const MDMarkdownPreviewTab = 1;
+const MDMarkdownSyntaxTab = 2;
 
 class EDMarkdownEditor extends React.Component {
 
@@ -115,6 +116,12 @@ class EDMarkdownEditor extends React.Component {
                     </li>
                     <li role="presentation"
                         className={classNames({
+                            'active': this.state.currentTab === MDMarkdownSyntaxTab
+                         })}>
+                         <a href="#" onClick={e => this.onOpenTab(e, MDMarkdownSyntaxTab)}>Formatting help</a>
+                    </li>
+                    <li role="presentation"
+                        className={classNames({
                             'active': this.state.currentTab === MDMarkdownPreviewTab,
                             'disabled': !this.state.value
                          })}>
@@ -136,6 +143,112 @@ class EDMarkdownEditor extends React.Component {
                             Read more (opens a new window)
                         </a>.
                     </small>
+                </div>
+                <div className={classNames({ 'hidden': this.state.currentTab !== MDMarkdownSyntaxTab })}>
+                    <p>
+                        Markdown is a lightweight markup language with plain text formatting syntax. 
+                        It is designed to make it easy for you to apply formatting to your text with 
+                        minimal impact on your content.
+                    </p>
+                    <p>
+                        We support the following Markdown syntax:
+                    </p>
+                    <table className="table table-striped">
+                        <thead>
+                            <th>Syntax</th>
+                            <th>Description</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><code>*Italics*</code></td>
+                                <td><em>Italics</em> for emphasis.</td>
+                            </tr>
+                            <tr>
+                                <td><code>**Bold text**</code></td>
+                                <td><b>Bold text</b> for emphasis.</td>
+                            </tr>
+                            <tr>
+                                <td><code>~~Strike-through~~</code></td>
+                                <td><s>Strike-through</s></td>
+                            </tr>
+                            <tr>
+                                <td><code>![An exclamation](exclamation-glyph.png)</code></td>
+                                <td>
+                                    <span className="glyphicon glyphicon-exclamation-sign" title="An exclamation" />. 
+                                    Displays an image with an alternate text (if the image fails to load).
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><code>[[tree]]</code></td>
+                                <td>Link to the dictionary entry for <em>tree</em>.</td>
+                            </tr>
+                            <tr>
+                                <td><code>[Link to trees](https://en.wikipedia.org/wiki/Tree)</code></td>
+                                <td>
+                                    <a href="https://en.wikipedia.org/wiki/Tree" target="_blank">
+                                        Link to trees
+                                    </a>
+                                    . Links to the specified page. 
+                                    Any external and internal addresses may be specified.
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <code>
+                                        * item 1<br />
+                                        * item 2<br />
+                                        &nbsp;&nbsp;&nbsp;* sub item 1<br />
+                                        * item 3
+                                    </code>
+                                </td>
+                                <td>
+                                    <ul>
+                                        <li>item 1</li>
+                                        <li>
+                                            item 2
+                                            <ul>
+                                                <li>sub item 1</li>
+                                            </ul>
+                                        </li>
+                                        <li>item 3</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <code>
+                                        1. run<br />
+                                        2. hide
+                                    </code>
+                                </td>
+                                <td>
+                                    <ol>
+                                        <li>run</li>
+                                        <li>hide</li>
+                                    </ol>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><code># Header</code></td>
+                                <td>1st level header. <em>Please use with care!</em></td>
+                            </tr>
+                            <tr>
+                                <td><code>## Header</code></td>
+                                <td>2nd level header. <em>Please use with care!</em></td>
+                            </tr>
+                            <tr>
+                                <td><code>### Header</code></td>
+                                <td>3rd level header. <em>Please use with care!</em></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <p>
+                        More information about Markdown 
+                        {' '}
+                        <a href="https://en.wikipedia.org/wiki/Markdown" target="_blank">
+                            can be found on Wikipedia
+                        </a>.
+                    </p>
                 </div>
                 <div className={classNames({ 'hidden': this.state.currentTab !== MDMarkdownPreviewTab })} 
                     ref={container => this.markupContainer = container}>
