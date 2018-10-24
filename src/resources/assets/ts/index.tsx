0229a6351167c7748bf7598f4797d5fba163f1d1
galadhremmin <zanathel@gmail.com>
2018-10-23 17:12:27 -0700

    Fixed unit tests

--- src/resources/assets/ts/index.tsx
+++ src/resources/assets/ts/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import '../sass/app.scss';
-import './di/config';
 
 import SearchContainer from './apps/book-browser/components/SearchContainer';
 
