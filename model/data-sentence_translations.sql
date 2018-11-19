4b1e6f99f042417840dfd52619869bf7a0717b1c
galadhremmin <zanathel@gmail.com>
2018-11-18 19:15:53 -0800

    Sentence translations for Elbereth Gilthoniel

---
+++ model/data-sentence_translations.sql
@@ -0,0 +1,10 @@
+
+insert into sentence_translations (sentence_id, sentence_number, paragraph_number, translation)
+    values (11, 10, 10, 'O Elbereth Starkindler,'),
+        (11, 10, 20, 'white-glittering, slanting down sparkling like a jewel,'),
+        (11, 10, 30, 'the glory of the starry host!'),
+        (11, 20, 40, 'Having gazed far away'),
+        (11, 20, 50, 'from the tree-woven lands of Middle-earth,'),
+        (11, 20, 60, 'to thee, Everwhite, I will sing,'),
+        (11, 20, 70, 'on this side of the Sea, here on this side of the Ocean!');
+
