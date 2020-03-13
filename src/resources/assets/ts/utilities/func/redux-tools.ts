ed45e17854f3e969d318a3ba41f97b9d91cc4239
galadhremmin <zanathel@gmail.com>
2020-03-12 20:33:42 -0700

    Minor server error fix

--- src/resources/assets/ts/utilities/func/redux-tools.ts
+++ src/resources/assets/ts/utilities/func/redux-tools.ts
@@ -2,5 +2,7 @@ import {
     compose,
 } from 'redux';
 
-export const composeEnhancers = (name: string): any =>
-    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ name: `ed-${name}-model` }) || compose;
+export const composeEnhancers = (name: string): any => {
+    const devTools = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+    return devTools ? devTools({ name: `ed-${name}-model` }) : compose;
+};
