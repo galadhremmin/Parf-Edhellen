6e79cb9f27f27e62e9da9d209ab5828355e01e1a
galadhremmin <zanathel@gmail.com>
2020-05-10 21:15:40 -0700

    Frontpage ad

--- src/resources/views/_shared/_ads.blade.php
+++ src/resources/views/_shared/_ads.blade.php
@@ -1,1 +1,21 @@
-<script type="text/javascript" src="@assetpath(/js/global-plugins-ads.js)"></script>
+<script>
+(() => {
+const props = {className:'adsbygoogle'};
+const dataset = {
+adClient:'ca-pub-8268364504414566',
+adFormat:'auto',
+fullWidthResponsive:true
+};
+window['ed.ads.config'] = {
+frontpage: {
+  props,
+  dataset: {
+    ...dataset,
+    adSlot:'6826878711'
+  }
+},
+_mounted: () => { (adsbygoogle = window.adsbygoogle || []).push({}); }
+};
+})();
+</script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
