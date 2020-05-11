271165ccd0bc000fca8f15ce454a1c815d7b737e
galadhremmin <zanathel@gmail.com>
2020-05-10 22:44:37 -0700

    Ad defs

--- src/resources/views/_shared/_ads.blade.php
+++ src/resources/views/_shared/_ads.blade.php
@@ -10,12 +10,30 @@ adFormat:'auto',
 fullWidthResponsive:true,
 };
 window['ed.ads.config'] = {
+flashcards: {
+  props,
+  dataset: Object.assign({
+    adSlot:'3964210950'
+  }, dataset)
+},
 frontpage: {
   props,
-  dataset: {
-    ...dataset,
+  dataset: Object.assign({
     adSlot:'6826878711'
-  }
+  }, dataset)
+},
+phrases: {
+  props,
+  dataset: Object.assign({
+    adSlot:'3237620511'
+  }, dataset)
+},
+glossary: {
+  props,
+  dataset: Object.assign({
+    adSlot:'9363211011',
+    adLayoutKey:'-gw-3+1f-3d+2z'
+  }, dataset)
 },
 _mount: () => { (adsbygoogle = window.adsbygoogle || []).push({}); }
 };
