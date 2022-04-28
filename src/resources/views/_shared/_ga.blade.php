2defdfe9e5f46d0660d4f1ad08bd6302b6955eba
galadhremmin <zanathel@gmail.com>
2022-04-28 13:52:54 -0700

    Updated Google analytics

--- src/resources/views/_shared/_ga.blade.php
+++ src/resources/views/_shared/_ga.blade.php
@@ -1,19 +1,17 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-26836717-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6J3WM5JEVV"></script>
 <script>
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'UA-26836717-1');
-window.addEventListener('ednavigate', function (ev) {
-gtag('config', 'UA-26836717-1', {'page_path':ev.detail.address});  
-gtag('event', 'page_view');
+gtag('config', 'G-6J3WM5JEVV');
+window.addEventListener('ednavigate',function(ev){
+gtag('config','G-6J3WM5JEVV',{'page_path':ev.detail.address});  
+gtag('event','page_view');
 });
 </script>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-8268364504414566",
-    enable_page_level_ads: true
-  });
+(adsbygoogle = window.adsbygoogle||[]).push({
+google_ad_client:'ca-pub-8268364504414566',
+enable_page_level_ads:true
+});
 </script>
