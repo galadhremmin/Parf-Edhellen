9e0a17a23e0703f9017a614ddb1a6545d6828b89
galadhremmin <zanathel@gmail.com>
2022-08-31 01:09:32 -0700

    Broken commit, just stashing for safety.

--- src/resources/assets/ts/apps/sentence-inspector/reducers/FragmentsReducer.ts
+++ src/resources/assets/ts/apps/sentence-inspector/reducers/FragmentsReducer.ts
@@ -34,13 +34,7 @@ const FragmentsReducer = (state: FragmentsReducerState = [], action: ISentenceRe
                 fragment: 'fragment',
                 glossId: 'glossId',
                 id: (v) => v.type === SentenceFragmentType.Word ? (v.id || 0) : 0,
-                inflections: (v) => {
-                    if (Array.isArray(v.inflections)) {
-                        return v.inflections.map<any>((i) => action.sentence.inflections[i.inflectionId]);
-                    }
-
-                    return action.sentence.inflections[v.id.toString(10)]
-                },
+                glossInflections: (v: ISentenceFragmentEntity) => v.glossInflections.map((i) => action.sentence.inflections[i.inflectionId]),
                 nextFragmentId: (v, i) => findNextFragmentId(v, fragments, i, 1),
                 previousFragmentId: (v, i) => findNextFragmentId(v, fragments, i, -1),
                 sentenceNumber: 'sentenceNumber',
