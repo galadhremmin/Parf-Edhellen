83407e91395bf505247879075a7590674178a421
galadhremmin <zanathel@gmail.com>
2022-11-10 21:48:12 -0800

    Wohops fix

--- src/resources/assets/ts/apps/discuss-feed/index._types.ts
+++ src/resources/assets/ts/apps/discuss-feed/index._types.ts
@@ -1,8 +1,10 @@
 import { ISubscriptionApi } from '@root/connectors/backend/ISubscriptionApi';
 import { FeedUrlFactory } from '@root/connectors/IFeedApi';
+import { IRoleManager } from '@root/security';
 
 export interface IProps {
     feedUrlFactory: FeedUrlFactory;
     groupId?: number;
+    roleManager: IRoleManager;
     subscriptionApi: ISubscriptionApi; 
 }
