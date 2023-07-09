e47c83034805232cc4aa0fe0e75105f682010641
galadhremmin <zanathel@gmail.com>
2023-07-08 21:31:59 -0700

    Added old source label

--- src/resources/assets/ts/apps/book-browser/components/GlossaryEntities/GlossGroupLabel.tsx
+++ src/resources/assets/ts/apps/book-browser/components/GlossaryEntities/GlossGroupLabel.tsx
@@ -4,16 +4,22 @@ const GlossGroupLabel = (props: IProps) => {
     const {
         glossGroupLabel,
         label: glossLabel,
+        isOld,
     } = props.gloss;
 
     const label = glossLabel ?? glossGroupLabel;
-    if (! label) {
+    if (! label && ! isOld) {
         return null;
     }
 
-    return <span className="gloss-word__neologism">
-        <span className="badge rounded-pill badge-sm bg-info position-absolute top-0 start-0 ms-1 translate-middle-y" title={label}>{label}</span>
+    return <span className="gloss-word__neologism position-absolute top-0 start-0 ms-1 translate-middle-y">
+        {label && <span className="badge rounded-pill badge-sm bg-info" title={label}>{label}</span>}{' '}
+        {!! isOld && <span className="badge rounded-pill badge-sm bg-danger" title="This gloss was imported from a dictionary that hasn't been updated for years.">Old source</span>}
     </span>;
+};
+
+GlossGroupLabel.defaultProps = {
+    isOld: false,
 };
 
 export default GlossGroupLabel;
