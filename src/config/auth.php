62be5b1782fdb275ce0f01839cef3a4fb263833e
galadhremmin <zanathel@gmail.com>
2024-06-05 00:13:52 -0700

    Session updates

--- src/config/auth.php
+++ src/config/auth.php
@@ -69,11 +69,6 @@ return [
             'driver' => 'eloquent',
             'model' => App\Models\Account::class,
         ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
 
     /*
