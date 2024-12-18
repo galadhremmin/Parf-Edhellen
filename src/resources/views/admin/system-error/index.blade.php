d97ef053d047ffba330090a72f9a1da169a3f139
galadhremmin <zanathel@gmail.com>
2024-12-17 22:06:14 -0800

    Added audit trail in admin view

--- src/resources/views/admin/system-error/index.blade.php
+++ src/resources/views/admin/system-error/index.blade.php
@@ -6,18 +6,32 @@
 <h1>Service errors</h1>
 {!! Breadcrumbs::render('system-error.index') !!}
 
-<div data-inject-module="system-log"
+<section data-inject-module="system-log"
      data-inject-prop-errors-by-week="@json($errorsByWeek)"
      data-inject-prop-error-categories="@json($errorCategories)"
      data-inject-prop-failed-jobs-by-week="@json($failedJobsByWeek)"
-     data-inject-prop-failed-jobs-categories="@json($failedJobsCategories)"></div>
+     data-inject-prop-failed-jobs-categories="@json($failedJobsCategories)"></section>
 
-<hr>
 
-<h2>Test connectivity</h2>
+<section class="card mb-3 shadow">
+   <div class="card-body">
+    @include('_shared._audit-trail', [
+      'auditTrail' => $auditTrailEntries
+    ])
+      <div class="d-flex justify-content-between">
+         <a class="btn btn-secondary" href="?audit_trail_page={{ max(0, $auditTrailPage - 1) }}">Load newer</a>
+         <a class="btn btn-secondary" href="?audit_trail_page={{ $auditTrailPage + 1 }}">Load older</a>
+      </div>
+   </div>
+</div>
+<section class="card mb-3 shadow">
+   <div class="card-body">
+      <h2>Test connectivity</h2>
 
-@foreach ([ 'IdentifiesPhrasesMonitor' ] as $component)
-   <a class="btn btn-secondary" href="{{ route('system-error.connectivity', [ 'component' => $component ]) }}">{{ $component }}</a>
-@endforeach
+      @foreach ([ 'IdentifiesPhrasesMonitor' ] as $component)
+         <a class="btn btn-secondary" href="{{ route('system-error.connectivity', [ 'component' => $component ]) }}">{{ $component }}</a>
+      @endforeach
+   </div>
+</section>
 
 @endsection
