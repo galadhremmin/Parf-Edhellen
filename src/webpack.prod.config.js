01144af206813f74a4a38f88edf3cafd02383162
galadhremmin <leonard@wickmark.se>
2025-07-17 00:04:35 -0700

    Stupid mistake...

--- src/webpack.prod.config.js
+++ src/webpack.prod.config.js
@@ -1,28 +1,25 @@
-const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.config');
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = Object.assign(
+module.exports = [
     baseConfig.filter(c => c.target === 'node')[0],
-    Object.assign(
-        baseConfig.filter(c => target === 'web')[0],
-        {
-            devtool: false,
-            optimization: {
-                minimize: true,
-                minimizer: [
-                    new TerserPlugin({
-                        parallel: true,
-                        terserOptions: {
-                            keep_classnames: true,
-                            keep_fnames: true,
-                            mangle: {
-                                reserved: ['Glaemscribe'],
-                            }
-                        },
-                    }),
-                ],
-            },
-        }
-    )
-);
+    {
+        ...baseConfig.filter(c => c.target === 'web')[0],
+        devtool: false,
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    parallel: true,
+                    terserOptions: {
+                        keep_classnames: true,
+                        keep_fnames: true,
+                        mangle: {
+                            reserved: ['Glaemscribe'],
+                        }
+                    },
+                }),
+            ],
+        },
+    }
+];
