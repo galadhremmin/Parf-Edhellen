535263794d118f75c0ca8f3dd4fedea3df928db5
galadhremmin <leonard@wickmark.se>
2025-07-18 09:11:25 -0700

    Updated to Laravel 12 & maintenance mode

---
+++ src/resources/views/errors/503.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Site Under Maintenance</title>
+<style>
+    body {
+        text-align: center;
+        padding: 150px;
+        background-color: #f4f4f4;
+        font-family: Arial, sans-serif;
+    }
+</style>
+<h1>We'll be back soon!</h1>
+<p>Sorry for the inconvenience, but we're performing some maintenance right now. We'll be back up shortly!</p>
