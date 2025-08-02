5e80e1f3372b8e62390288c838d36a1ed23aec3a
galadhremmin <leonard@wickmark.se>
2025-08-02 15:56:31 -0700

    Removed unnecessary includes

--- src/app/Http/Controllers/Contributions/LegacyGlossContributionController.php
+++ src/app/Http/Controllers/Contributions/LegacyGlossContributionController.php
@@ -2,20 +2,9 @@
 
 namespace App\Http\Controllers\Contributions;
 
-use App\Adapters\BookAdapter;
-use App\Http\Controllers\Abstracts\Controller;
-use App\Http\Controllers\Traits\CanMapGloss;
-use App\Http\Controllers\Traits\CanValidateGloss;
-use App\Models\Account;
 use App\Models\Contribution;
-use App\Models\LexicalEntry;
 use App\Models\LexicalEntryDetail;
-use App\Models\Sense;
 use App\Models\Gloss;
-use App\Models\Word;
-use App\Repositories\LexicalEntryInflectionRepository;
-use App\Repositories\LexicalEntryRepository;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class LegacyGlossContributionController extends LexicalEntryContributionController
