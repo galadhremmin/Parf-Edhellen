8fed50ddae0c858c95cf64a4bc8b652fdae6f36e
galadhremmin <leonard@wickmark.se>
2025-08-09 01:08:50 -0700

    Corrected an incorrect link

--- src/resources/views/contribution/index.blade.php
+++ src/resources/views/contribution/index.blade.php
@@ -14,7 +14,7 @@
       The process for getting your contribution approved has the following steps:
     </p>
     <ol>
-      <li>You submit as much information about your gloss or phrase as possible via the forms below.</li>
+      <li>You submit as much information about your lexical entry or phrase as possible via the forms below.</li>
       <li>An administrator reviews the contribution and requests additional information, if necessary. This usually happens because you have provided insufficient source information.</li>
       <li>Once approved, the contribution is created in your name and highlighted on the front page! You get full attribution for your work.</li>
     </ol>
@@ -22,7 +22,7 @@
       To get started, click on the button that corresponds to what you would like to add below.
     </p>
     <div class="text-center">
-      <a href="{{ route('contribution.create', ['morph' => 'gloss']) }}" class="btn btn-primary">Add gloss</a>
+      <a href="{{ route('contribution.create', ['morph' => 'lexical_entry']) }}" class="btn btn-primary">Add entry</a>
       <a href="{{ route('contribution.create', ['morph' => 'sentence']) }}" class="btn btn-primary">Add phrase</a>
     </div>
   </div>
