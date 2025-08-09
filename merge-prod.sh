afc15c725d31eb19f8355068c754847a28406180
galadhremmin <leonard@wickmark.se>
2025-08-08 23:52:46 -0700

    Abandoning parf-edhellen-prod branch

--- merge-prod.sh
+++ merge-prod.sh
@@ -2,7 +2,7 @@
 
 git checkout master
 git push
-git checkout parf-edhellen-prod
+git checkout parf-edhellen-prod-v2
 git pull origin master
-git push origin parf-edhellen-prod
+git push origin parf-edhellen-prod-v2
 git checkout master
