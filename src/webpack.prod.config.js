8c9b3a243435b6ec6bd85eb51579bdb014b85dda
galadhremmin <leonard@wickmark.se>
2025-11-15 20:38:07 -0800

    Added support for optional SSR

--- src/webpack.prod.config.js
+++ src/webpack.prod.config.js
@@ -2,27 +2,32 @@ const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.config');
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = [
-    baseConfig.filter(c => c.target === 'node')[0],
-    merge(
-        baseConfig.filter(c => c.target === 'web')[0],
-        {
-            devtool: false,
-            optimization: {
-                minimize: true,
-                minimizer: [
-                    new TerserPlugin({
-                        parallel: true,
-                        terserOptions: {
-                            keep_classnames: true,
-                            keep_fnames: true,
-                            mangle: {
-                                reserved: ['Glaemscribe'],
-                            }
-                        },
-                    }),
-                ],
-            },
+const serverConfig = baseConfig.find(c => c.target === 'node');
+const clientConfig = merge(
+    baseConfig.find(c => c.target === 'web'),
+    {
+        devtool: false,
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    parallel: true,
+                    terserOptions: {
+                        keep_classnames: true,
+                        keep_fnames: true,
+                        mangle: {
+                            reserved: ['Glaemscribe'],
+                        }
+                    },
+                }),
+            ],
         },
-    ),
+    },
+);
+
+module.exports = serverConfig ? [
+    serverConfig,
+    clientConfig,
+] : [
+    clientConfig,
 ];
