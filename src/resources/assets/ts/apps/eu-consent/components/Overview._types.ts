97506534caff9044e12bc4fef62c72e66a6ac5ec
galadhremmin <leonard@wickmark.se>
2025-11-16 00:44:29 -0800

    Removed EU's rubbish

--- src/resources/assets/ts/apps/eu-consent/components/Overview._types.ts
+++
@@ -1,5 +0,0 @@
-import type { ComponentEventHandler } from '@root/components/Component._types';
-
-export interface IProps {
-    onSettings: ComponentEventHandler<void>;
-}
--- src/resources/assets/ts/apps/eu-consent/components/Settings._types.ts
+++
@@ -1,6 +0,0 @@
-import type { ComponentEventHandler } from '@root/components/Component._types';
-
-export interface IProps {
-    consentedUseCases: string[];
-    onConsentedUseCasesChange: ComponentEventHandler<string[]>;
-}
