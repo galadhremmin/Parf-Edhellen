534d529969110808a2034ac61082f99d00e61f7b
galadhremmin <leonard@wickmark.se>
2026-01-15 22:54:46 -0800

    Fixed unit test

--- compile-prod.sh
+++ compile-prod.sh
@@ -3,10 +3,10 @@
 cd src
 git checkout master
 ./vendor/bin/phpstan analyse --memory-limit=2G
-./vendor/bin/phpunit
+php artisan test
 git checkout parf-edhellen-prod-v2
 git pull
-./vendor/bin/phpunit
+php artisan test
 npm run production
 git checkout master
 cd ..
