8abcd79d7b204243be7b2cef7e7b41a354245a87
galadhremmin <leonard@wickmark.se>
2026-01-21 21:26:47 -0800

    Restored feed

--- src/routes/api-routes/api-discuss-feed.php
+++ src/routes/api-routes/api-discuss-feed.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Api\v3\DiscussFeedApiController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    'prefix' => 'discuss/feed',
+    'prefix' => API_PATH.'/discuss/feed',
 ], function () {
     Route::get('posts', [DiscussFeedApiController::class, 'getPosts'])
         ->name('api.discuss-feed.posts');
