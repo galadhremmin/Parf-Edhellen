c87134eb37ae251fc26787ad12c3fe926cbcd9b4
galadhremmin <leonard@wickmark.se>
2026-03-15 20:40:30 -0700

    Buy me a coffee donation feature

---
+++ src/resources/views/_shared/_buy-me-a-coffee.blade.php
@@ -0,0 +1,9 @@
+<div class="ed-support-cta card shadow-sm mt-3 mb-3">
+  <div class="card-body">
+    <h5 class="ed-support-cta__heading card-title">{{ __('donations.buymeacoffee.heading') }}</h5>
+    <p class="ed-support-cta__text card-text text-body-secondary">{{ __('donations.buymeacoffee.text') }}</p>
+    <a href="https://www.buymeacoffee.com/elfdict.com" target="_blank" rel="noopener noreferrer" class="btn btn-primary ed-support-cta__link">
+      {{ __('donations.buymeacoffee.cta') }}
+    </a>
+  </div>
+</div>
