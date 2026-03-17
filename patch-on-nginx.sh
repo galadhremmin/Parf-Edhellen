285179f4f0b875b2f29ec7844c63e16568181dde
galadhremmin <leonard@wickmark.se>
2026-03-17 16:03:08 -0700

    Improvements to version management

--- patch-on-nginx.sh
+++ patch-on-nginx.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+# Parse --version=X argument
+for arg in "$@"; do
+  case $arg in
+    --version=*)
+      ED_VERSION="${arg#*=}"
+      shift
+      ;;
+  esac
+done
+
 sudo -u www-data git pull
 cd src
+
+# Update ED_VERSION in .env if --version was provided
+if [ -n "$ED_VERSION" ]; then
+  if grep -q "^ED_VERSION=" .env 2>/dev/null; then
+    sed -i "s/^ED_VERSION=.*/ED_VERSION=${ED_VERSION}/" .env
+  else
+    echo "ED_VERSION=${ED_VERSION}" >> .env
+  fi
+fi
+
 if ! [ -z "$1" ]
   then
     mv -v $1 public/
