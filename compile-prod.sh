4c922fc578a18f1154a246fd18626c13fd889668
galadhremmin <leonard@wickmark.se>
2026-09-13 10:32:17 -0700

    Updated compilation script

--- compile-prod.sh
+++ compile-prod.sh
@@ -5,7 +5,8 @@ git checkout master
 ./vendor/bin/phpstan analyse --memory-limit=2G
 php artisan test
 git checkout parf-edhellen-prod-v2
-git pull
+# --ff-only overrides this repo's pull.ff=no, which would otherwise merge on every pull
+git pull --ff-only
 php artisan test
 npm run production
 git checkout master
