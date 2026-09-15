4b7d2e2ffb56b71fefb62e5073e548fb16c3605f
galadhremmin <leonard@wickmark.se>
2026-09-15 14:13:59 -0700

    Improved admin tool for glossary

--- src/app/Http/Controllers/Resources/GlossController.php
+++ src/app/Http/Controllers/Resources/GlossController.php
@@ -6,8 +6,9 @@ use App\Adapters\BookAdapter;
 use App\Http\Controllers\Abstracts\Controller;
 use App\Http\Controllers\Traits\CanMapGloss;
 use App\Http\Controllers\Traits\CanValidateGloss;
-use App\Models\LexicalEntry;
 use App\Models\Language;
+use App\Models\LexicalEntry;
+use App\Models\Speech;
 use App\Repositories\LexicalEntryRepository;
 use Illuminate\Http\Request;
 
@@ -47,18 +48,38 @@ class GlossController extends Controller
 
     public function listForLanguage(Request $request, int $id)
     {
+        $filters = $request->validate([
+            'word' => 'nullable|string|max:128',
+            'gloss' => 'nullable|string|max:128',
+            'sense' => 'nullable|string|max:128',
+            'speech_id' => 'nullable|integer',
+            'missing' => 'nullable|in:source,sense',
+        ]);
+
         $language = Language::findOrFail($id);
-        $lexicalEntries = LexicalEntry::active()
-            ->where('language_id', $id)
-            ->join('words', 'words.id', 'lexical_entries.word_id')
-            ->orderBy('words.word', 'asc')
-            ->with('glosses', 'account', 'sense.word', 'speech', 'keywords', 'word')
-            ->select('lexical_entries.*')
-            ->paginate(30);
+        $lexicalEntries = $this->_lexicalEntryRepository->getLexicalEntriesForLanguage($id, $filters);
+        $speeches = Speech::orderBy('name')
+            ->select('id', 'name')
+            ->get();
 
         return view('admin.gloss.list', [
             'language' => $language,
             'glosses' => $lexicalEntries,
+            'filters' => $filters,
+            'speeches' => $speeches,
         ]);
+    }
+
+    public function updateSense(Request $request, int $id)
+    {
+        $data = $request->validate([
+            'sense' => 'required|string|max:255',
+        ]);
+
+        $lexicalEntry = LexicalEntry::active()->findOrFail($id);
+        $this->_lexicalEntryRepository->saveSense($lexicalEntry, trim($data['sense']));
+
+        return redirect()->to(url()->previous().'#lexical-entry-'.$lexicalEntry->id)
+            ->with('updated_sense_id', $lexicalEntry->id);
     }
 }
